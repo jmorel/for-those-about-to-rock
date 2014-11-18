35d03dcf7a1d6bd338ac4ec23a88c5899a7ccd62
Jérémy Morel <morel.jeremy@gmail.com>
2014-11-18 22:25:51 +0100

    moving to postgres

--- ftatr/settings.py
+++ ftatr/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'ftatr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ftatr',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': 'localhost',
-        'PORT': 3306
+        'PORT': 5432
     }
 }
 
@@ -99,9 +99,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'ftatr/static'),
 )
 
 # Media files (user uploaded)
