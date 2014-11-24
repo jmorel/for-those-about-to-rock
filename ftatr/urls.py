3fc3d5c9b8b3a9e869cf2aa18598a72dd923a938
Jérémy Morel <morel.jeremy@gmail.com>
2014-11-24 09:11:15 +0100

    Added rocking chairs feed to layout

--- ftatr/urls.py
+++ ftatr/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^feed/rocking-chairs-latest$', feeds.LatestRockingChairsFeed()),
+    url(r'^feed/rocking-chairs-latest$', feeds.LatestRockingChairsFeed(), name='rocking-chair-feed'),
     url(r'^$', views.index, name='rocking-chair-index'),
     url(r'^rocking-chair/(?P<slug>\w+)$', views.show, name='rocking-chair-show'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
