c2e49bf017962915d5686d31d1bf4119059a80db
Jérémy Morel <morel.jeremy@gmail.com>
2014-11-26 08:42:35 +0100

    root route redirects to rocking_chair.index

--- ftatr/urls.py
+++ ftatr/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
 from ftatr import settings
 
 urlpatterns = patterns('',
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'ftatr.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', RedirectView.as_view(url='/rocking-chair')),
     url(r'^rocking-chair/', include('rocking_chair.urls', namespace='rocking_chair')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
