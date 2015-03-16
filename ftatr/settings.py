f95238e9051e43811876491769d353153e541731
nemo <nemo@wearetheshops.com>
2015-03-16 15:37:42 +0100

    wip mobile nav

--- ftatr/settings.py
+++ ftatr/settings.py
@@ -1,104 +1,18 @@
-"""
-Django settings for for_those_about_to_rock project.
+from ftatr.common_settings import *
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
+# keep to false for prod environment
+DEBUG = True
+TEMPLATE_DEBUG = True
 
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm_p!ki=cue6*^l6ao)=3!ek1u#j6rg6+4%n7gu6wxf-8%k+$q3'
-FACEBOOK_APP_ID = '351060788405753'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-TEMPLATE_DEBUG = False
+SECRET_KEY = 'secret'
 
 ALLOWED_HOSTS = ['.forthoseabouttorock.io', 'forthoseabouttorock.hexagonal.io']
 
-
-# Application definition
-
-INSTALLED_APPS = (
-    'suit',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_jinja',
-    'easy_thumbnails',
-    'django_jinja.contrib._easy_thumbnails',
-    'haystack',
-    'django.contrib.sites',
-    'django.contrib.sitemaps',
-    'ftatr',
-    'rocking_chair',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'ftatr.urls'
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.app_directories.Loader',
-    'django_jinja.loaders.FileSystemLoader',
-    'django_jinja.loaders.AppLoader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
-    'django.contrib.auth.context_processors.auth',
-    'ftatr.context_processors.facebook'
-)
-
-DEFAULT_JINJA2_TEMPLATE_EXTENSION = '.jinja2'
-
-WSGI_APPLICATION = 'ftatr.wsgi.application'
-
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-
-# Thumbnails
-THUMBNAIL_ALIASES = {
-    '': {
-        # project wide target
-        '540x540': {
-            'size': (540, 540),
-        },
-        '50x50': {
-            'size': (50, 50),
-        }
-    }
-}
-
-
-# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ftatr',
-        'USER': 'postgres',
+        'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': 5432
@@ -113,11 +27,6 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
-# Sitemap.xml
-SITE_ID = 1
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -125,26 +34,30 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
-USE_TZ = False
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'ftatr/static'),
-)
-
-# Media files (user uploaded)
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
 
-# WARNING
-# this needs to be at the very end of the file so that variables are overriden
-try:
-    from ftatr.settings_local import *
-except ImportError:
-    pass
+STATIC_URL = '/static/'
+
+
+# Twitter
+TWITTER_TOKEN = '2958240303-MGsdC8JUQX0q7VQod9fC7ieukVHbkmhVvO2VYzz'
+TWITTER_TOKEN_SECRET = 'NxM84qMXS8UiP2K3SlbSgt6I8BerAR5tCQ7JeISij82ac'
+TWITTER_CONSUMER_SECRET = 'Hz8wnrx6XqFC54KXvOJBax6rAZiua1jOzbTxYhlEiw7UyblOgf'
+TWITTER_CONSUMER_KEY = 'sBSJX9L94dSOaG7WxX1L0SHYl'
+
+# Facebook
+FACEBOOK_APP_ID = '351060788405753'
+
+# Admins
+ADMINS = (('Jérémy', 'jeremy@forthoseabouttorock.io'), )
+
+# Sites framework definition
+SITE_ID = 1
+
+# Email
+EMAIL_HOST = ''
+EMAIL_USER = ''
+EMAIL_PORT = ''
+EMAIL_HOST_PASSWORD = ''
