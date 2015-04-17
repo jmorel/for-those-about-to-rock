24e47f28b02879648c668ecd6fece9b60e42fd4f
Jérémy Morel <morel.jeremy@gmail.com>
2015-04-17 10:13:34 +0200

    fix analytics requests

--- anthology/views/analytics.py
+++ anthology/views/analytics.py
@@ -16,29 +16,23 @@ def index(request):
 def countries_serie():
     countries = {}
 
-    designers = Designer.objects \
-        .exclude(rocking_chairs__published_at__gte=datetime.datetime.now()) \
-        .exclude(rocking_chairs__published_at=None)
+    designers = Designer.objects.with_published_rocking_chairs()
     for designer in designers.all():
         for country in designer.nationalities.all():
             increment_serie(serie=countries, key=country.code, field='designers')
 
-    manufacturers = Manufacturer.objects \
-        .exclude(rocking_chairs__published_at__gte=datetime.datetime.now()) \
-        .exclude(rocking_chairs__published_at=None)
+    manufacturers = Manufacturer.objects.with_published_rocking_chairs()
     for manufacturer in manufacturers.all():
         increment_serie(serie=countries, key=manufacturer.country.code, field='manufacturers')
 
-    rocking_chairs = RockingChair.objects \
-        .exclude(published_at__gte=datetime.datetime.now()) \
-        .exclude(published_at=None) \
-        .order_by('-published_at')
+    rocking_chairs = RockingChair.objects.published().order_by('-published_at')
     for rocking_chair in rocking_chairs.all():
         c1 = designers_countries(rocking_chair.designers.all())
         c2 = manufacturers_countries(rocking_chair.manufacturers.all())
         c = c1 + c2
         for country in c:
-            increment_serie(serie=countries, key=country.code, field='rocking_chairs')
+            if country:
+                increment_serie(serie=countries, key=country.code, field='rocking_chairs')
 
     serie = [{'name': code,
               'x': country['designers'],
@@ -66,22 +60,14 @@ def increment_serie(serie, key, field, fields=('rocking_chairs', 'designers', 'm
 
 
 def designers_serie():
-    designers = Designer.objects \
-        .annotate(num_rocking_chairs=Count('rocking_chairs')) \
-        .exclude(rocking_chairs__published_at__gte=datetime.datetime.now()) \
-        .exclude(rocking_chairs__published_at=None) \
-        .exclude(num_rocking_chairs__lte=1) \
-        .order_by('-num_rocking_chairs')
-    serie = [[designer.full_name, designer.num_rocking_chairs] for designer in designers[:20]]
+    designers = Designer.objects.with_published_rocking_chairs()
+    serie = [[designer.full_name, designer.published_rocking_chairs.count()] for designer in designers[:20]]
+    serie.sort(key=lambda designer: designer[1], reverse=True)
     return serie
 
 
 def manufacturers_serie():
-    manufacturers = Manufacturer.objects \
-        .annotate(num_rocking_chairs=Count('rocking_chairs')) \
-        .exclude(rocking_chairs__published_at__gte=datetime.datetime.now()) \
-        .exclude(rocking_chairs__published_at=None) \
-        .exclude(num_rocking_chairs__lte=1) \
-        .order_by('-num_rocking_chairs')
-    serie = [[manufacturer.name, manufacturer.num_rocking_chairs] for manufacturer in manufacturers[:20]]
+    manufacturers = Manufacturer.objects.with_published_rocking_chairs()
+    serie = [[manufacturer.name, manufacturer.published_rocking_chairs.count()] for manufacturer in manufacturers[:20]]
+    serie.sort(key=lambda manufacturer: manufacturer[1], reverse=True)
     return serie
